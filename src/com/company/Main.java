0294c3c6b733ecfc8908bcbd1ead7f518603a8d3
赵刚 <18555386997@163.com>
2017-05-24 11:29:48 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("什么啊");
+        System.out.println(true);
+        int a = 10;
+        int b = 20;
+        System.out.print(a+b);
+
+    }
+}
